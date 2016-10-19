092d6b8f69baa7a367d1a3319d0cb3c207303d11
Administrator <Administrator@lenovo17>
2016-10-19 17:05:51 +0800

    a

--- TestWebProject/src/org/neojo/dao/DBH.java
+++ TestWebProject/src/org/neojo/dao/DBH.java
@@ -1,5 +1,23 @@
 package org.neojo.dao;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class DBH {
-	
+	public Connection getConn() throws FileNotFoundException, IOException, ClassNotFoundException, SQLException{
+		Properties config = new Properties();
+		config.load(new FileInputStream(new File("./config/db.properties")));
+		String url = "jdbc:mysql://"+config.getProperty("ip")+"/"+config.getProperty("dbname");
+		String user = config.getProperty("user");
+		String password = config.getProperty("password");
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection conn = DriverManager.getConnection(url,user,password);
+		return conn;
+	}
 }
