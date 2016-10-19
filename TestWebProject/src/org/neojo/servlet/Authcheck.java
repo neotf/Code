e3a6d1c1028aebb75e9956e5bd67cf6e5d821d14
Administrator <Administrator@lenovo17>
2016-10-19 16:40:11 +0800

    tomcat

---
+++ TestWebProject/src/org/neojo/servlet/Authcheck.java
@@ -0,0 +1,25 @@
+package org.neojo.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class Authcheck extends HttpServlet {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("utf-8");
+		
+		Cookie cookies = new Cookie("", "");
+		resp.addCookie(cookies);
+	}
+}
